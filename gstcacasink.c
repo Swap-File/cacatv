7a6907585338e09999bc4b15588ec0ba368fa59a
swap-file <bigmarkslp@gmail.com>
2018-08-05 11:20:54 -0500

    typo

--- gstcacasink.c
+++ gstcacasink.c
@@ -423,5 +423,5 @@ plugin_init (GstPlugin * plugin)
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     libcaca,
-    "Colored ASCII Art video sink",
+    "Colored ASCII Art video sink & filter",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
