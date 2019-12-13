32d3f77e5abb5affba205fd8904d7158e85ad5bd
swap-file <bigmarkslp@gmail.com>
2019-12-13 11:58:59 -0600

    Update gstcacasink.c

--- gstcacasink.c
+++ gstcacasink.c
@@ -420,6 +420,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    libgstcacasink,
+    cacasink,
     "Colored ASCII Art video sink & filter",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
