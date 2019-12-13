3b2713748c41e34f427b4fbf92cfc2efd299b0bc
swap-file <bigmarkslp@gmail.com>
2019-12-12 21:04:11 -0600

    1.14 Naming
    Adjust naming for 1.14, add binary for Pi4, and adjusted makefile

--- gstcacasink.c
+++ gstcacasink.c
@@ -40,8 +40,6 @@
 #endif
 
 #include <string.h>
-#include <sys/time.h>
-
 #include "gstcacasink.h"
 #include "gstcacatv.h"
 
@@ -422,6 +420,6 @@ plugin_init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    libcaca,
+    libgstcacasink,
     "Colored ASCII Art video sink & filter",
     plugin_init, VERSION, GST_LICENSE, GST_PACKAGE_NAME, GST_PACKAGE_ORIGIN)
